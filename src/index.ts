f9aefdec1dec76321ea44b0c7dd243153ad7d118
addehd <hej@adolfo.se>
2023-05-30 13:13:20 +0200

    moved logger to lib

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
 import express, { Request, Response } from 'express'
-import winston from 'winston'
+import { logger } from './lib/logger'
 
 const app = express()
-
-console.log('Hello World!')
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
-})
-
-// If not in production log `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest })`
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }))
-}
 
 app.get('/', (req: Request, res: Response) => {
   logger.info('Handling request for /')
