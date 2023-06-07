a10be4e0c0ea3971e9cf00c053bb3a67c3d24136
addehd <hej@adolfo.se>
2023-06-07 15:24:15 +0200

    testing with logger

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const logger_1 = require("./lib/logger");
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
+    logger_1.logger.info('Handling request for /');
     res.send('Hello World!');
 });
-app.listen(3000, () => {
-    console.log('App is listening on port 3000');
+app.listen(3011, () => {
+    logger_1.logger.info('App is listening on port 3011');
 });
